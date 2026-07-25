f472566ec12eb73799f99eb91c8a5b4d9c6ff8b5
axl <axelleonellifschitz@gmail.com>
2026-07-25 14:39:01 +0000

    chore: setting up platformio.ini

--- nodo-tanque/src/main.cpp
+++ nodo-tanque/src/main.cpp
@@ -2,9 +2,11 @@
 
 void setup() {
     Serial.begin(115200);
+    delay(1000);
     Serial.println("Nodo Angamed arrancando...");
+    Serial.printf("RAM libre: %d bytes\n", ESP.getFreeHeap());
+    Serial.printf("PSRAM total: %d bytes\n", ESP.getPsramSize());
+    Serial.printf("PSRAM libre: %d bytes\n", ESP.getFreePsram());
 }
 
-void loop() {
-    // por ahora vacío
-}
+void loop() {}
