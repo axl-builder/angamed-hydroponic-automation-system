b8e70aeb1a7061aaade583e34b107229dbe05161
axl <axelleonellifschitz@gmail.com>
2026-07-27 22:30:46 +0000

    feat: WiFi connection con timeout y secrets separados

--- nodo-tanque/src/main.cpp
+++ nodo-tanque/src/main.cpp
@@ -1,12 +1,32 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include "secrets.h"
+
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to WiFi ..");
+
+
+  unsigned long inicio = millis();
+  while (WiFi.status() != WL_CONNECTED) {
+    if (millis() - inicio > 20000) {
+        Serial.print("WiFi timeout - no se pudo conectar");
+        break;
+    }
+    Serial.print('.');
+    delay(1000);
+  }
+    Serial.println(WiFi.localIP());
+}
 
 void setup() {
-    Serial.begin(115200);
-    delay(1000);
-    Serial.println("Nodo Angamed arrancando...");
-    Serial.printf("RAM libre: %d bytes\n", ESP.getFreeHeap());
-    Serial.printf("PSRAM total: %d bytes\n", ESP.getPsramSize());
-    Serial.printf("PSRAM libre: %d bytes\n", ESP.getFreePsram());
+  Serial.begin(115200);
+  initWiFi();
+  Serial.print("RSSI: ");
+  Serial.println(WiFi.RSSI());
 }
+
 
 void loop() {}
