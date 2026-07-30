72ac3dfaba6a253b7fa333ec6dd2b6c74e713e4d
axl <axelleonellifschitz@gmail.com>
2026-07-30 03:22:01 +0000

    chore: Agregando libreria ArduinoJson

--- nodo-tanque/.pio/build/esp32-s3-devkitc-1/FrameworkArduino/libb64/cdecode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32-s3-devkitc-1/FrameworkArduino/libb64/cdecode.c.o: \
- /home/axl_builder/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
- /home/axl_builder/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
